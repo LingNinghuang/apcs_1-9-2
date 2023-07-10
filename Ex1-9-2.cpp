28c7e8cddd1a6a56548a9a841906e0182de4a3cf
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-10 14:42:04 +0800

    Create Ex1-9-2.cpp

---
+++ Ex1-9-2.cpp
@@ -0,0 +1,13 @@
+//operators
+#include <stdio.h>
+int main() {
+    int x = 24, y = 11;
+    printf("%d\n", x + y);
+    printf("%d\n", x - y);
+    printf("%d\n", x * y);
+    printf("%d\n", x / y);
+    printf("%d\n", x % y);
+    printf("%d\n", ++x);
+    printf("%d", --y);
+    return 0;
+}
